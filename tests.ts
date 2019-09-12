81f4708864a883855d70b1cf93aeff2a5b6017ac
Maker's Box <648.ken@gmail.com>
2019-09-12 12:29:36 -0700

    Added servo

--- tests.ts
+++ tests.ts
@@ -1,18 +1,1 @@
-// tests go here; this will not be compiled when this package is used as a library
-input.onButtonPressed(Button.A, () => {
-    turtle.invert(Motor.Left, false)
-    turtle.invert(Motor.Left, false)
-    turtle.setMotorSpeed(Motor.Left, MotorDirection.Forward, 80)
-    turtle.setMotorSpeed(Motor.Right, MotorDirection.Forward, 80)
-    turtle.enable(MotorPower.On)
-})
-input.onButtonPressed(Button.B, () => {
-    turtle.invert(Motor.Left, false)
-    turtle.invert(Motor.Left, false)
-    turtle.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 20)
-    turtle.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 20)
-    turtle.enable(MotorPower.On)
-})
-input.onButtonPressed(Button.AB, () => {
-    turtle.enable(MotorPower.Off)
-})
+ 
