cfefafe9095c40641d09927f4d18ea13478ce2dd
Maker's Box <648.ken@gmail.com>
2019-09-12 11:01:41 -0700

    Updates from web

--- turtle.cpp
+++ turtle.cpp
@@ -4,8 +4,18 @@
 
 using namespace pxt;
 
-uint16_t steps_rev = 512
-float wheel_dia = 72
+int steps_rev = 512;
+float wheel_dia = 72;
+int steps_rev=512;      //   512 for 64x gearbox, 128 for 16x gearbox
+int delay_time=4;       //   time between steps in ms
+int R_stepper_pins[] = {0, 7, 1, 6}; 
+int L_stepper_pins[] = {8, 10, 13, 9};      //org->pink->blue->yel
+
+int fwd_mask[][4] =  {{1, 0, 1, 0},
+                      {0, 1, 1, 0},
+                      {0, 1, 0, 1},
+                      {1, 0, 0, 1}};
+
 
 
 namespace turtle {
@@ -13,10 +23,24 @@ namespace turtle {
     * Calculates the number of stepper steps for a given distance in mm.
     */
     //%
-    uint16_t calc_steps(int16_t distance) {
+    int calc_steps(int distance) {
 	  float steps = distance * steps_rev / (wheel_dia * 3.1412);
 	  return int(steps);  
     }
+
+    void forward(float distance){
+    int steps = step(distance);
+    for(int step=0; step<steps; step++){
+        for(int mask=0; mask<4; mask++){
+        for(int pin=0; pin<4; pin++){
+            digitalWrite(L_stepper_pins[pin], fwd_mask[mask][pin]);
+            //digitalWrite(R_stepper_pins[pin], rev_mask[mask][pin]);
+        }
+        delay(delay_time);
+        } 
+    }
+}
+
 
 }
 
--- turtle.ts
+++ turtle.ts
@@ -22,14 +22,89 @@ enum MotorPower {
 /**
  * Functions to operate the turtle
  */
-//% color=#f44242 icon="\uf1b9" block="turtle"
+//% color=#0b9630 icon="\f188" block="turtle"
 namespace turtle {
+    let patterns = [[1, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 1], [1, 0, 0, 1]]
+    let pattern: number[] = []
+    let steps = 0
+    let wheel_dia = 0
+    wheel_dia = 52
+    let wheel_base = 74
+
+    //% block="set wheel_diameter=%wheel_dia (mm)"
+    //% val.defl=52 blockId="turtle_set_dia"
+    export function set_wheel_dia(val: number) {
+        wheel_dia = val
+    }
+
+    //% block="set wheel_base=%wheel_base (mm)"
+    //% val.defl=74 blockId="turtle_set_base"
+    export function set_wheel_base(val: number) {
+        wheel_base = val
+    }
+
+
+    /**
+     *  Distance to move the turtle backwards in millimeters.
+     */
+    //% block="backwards %distance (mm)" blockId="turtle_backwards"
+    //% distance.min=0 distance.defl=100
+    export function backwards(distance: number) {
+        steps = Math.round(distance * 512 / (wheel_dia * 3.1412))
+        for (let i = 0; i < steps; i++) {
+            for (let j = 0; j <= 4; j++) {
+                pattern = [1, 0, 1, 0]
+                pins.digitalWritePin(DigitalPin.P0, pattern[0])
+                pins.digitalWritePin(DigitalPin.P7, pattern[1])
+                pins.digitalWritePin(DigitalPin.P1, pattern[2])
+                pins.digitalWritePin(DigitalPin.P6, pattern[3])
+                pins.digitalWritePin(DigitalPin.P9, pattern[0])
+                pins.digitalWritePin(DigitalPin.P13, pattern[1])
+                pins.digitalWritePin(DigitalPin.P10, pattern[2])
+                pins.digitalWritePin(DigitalPin.P8, pattern[3])
+                basic.pause(2)
+                pattern = [0, 1, 1, 0]
+                pins.digitalWritePin(DigitalPin.P0, pattern[0])
+                pins.digitalWritePin(DigitalPin.P7, pattern[1])
+                pins.digitalWritePin(DigitalPin.P1, pattern[2])
+                pins.digitalWritePin(DigitalPin.P6, pattern[3])
+                pins.digitalWritePin(DigitalPin.P9, pattern[0])
+                pins.digitalWritePin(DigitalPin.P13, pattern[1])
+                pins.digitalWritePin(DigitalPin.P10, pattern[2])
+                pins.digitalWritePin(DigitalPin.P8, pattern[3])
+                basic.pause(2)
+                pattern = [0, 1, 0, 1]
+                pins.digitalWritePin(DigitalPin.P0, pattern[0])
+                pins.digitalWritePin(DigitalPin.P7, pattern[1])
+                pins.digitalWritePin(DigitalPin.P1, pattern[2])
+                pins.digitalWritePin(DigitalPin.P6, pattern[3])
+                pins.digitalWritePin(DigitalPin.P9, pattern[0])
+                pins.digitalWritePin(DigitalPin.P13, pattern[1])
+                pins.digitalWritePin(DigitalPin.P10, pattern[2])
+                pins.digitalWritePin(DigitalPin.P8, pattern[3])
+                basic.pause(2)
+                pattern = [1, 0, 0, 1]
+                pins.digitalWritePin(DigitalPin.P0, pattern[0])
+                pins.digitalWritePin(DigitalPin.P7, pattern[1])
+                pins.digitalWritePin(DigitalPin.P1, pattern[2])
+                pins.digitalWritePin(DigitalPin.P6, pattern[3])
+                pins.digitalWritePin(DigitalPin.P9, pattern[0])
+                pins.digitalWritePin(DigitalPin.P13, pattern[1])
+                pins.digitalWritePin(DigitalPin.P10, pattern[2])
+                pins.digitalWritePin(DigitalPin.P8, pattern[3])
+                basic.pause(2)
+            }
+        }
+    }
+
+
 	/**
 	 * Sets the speed and direction of either the left motor or the right motor.
      * @param motor the motor to act on
      * @param direction forward or backward
      * @param speed percent of maximum speed, eg: 50
 	 */
+
     //% blockId="motobit_setMotor" block="move %motor_number motor|motor %path|at %speed|%"
     //% speed.min=0 speed.max=100
     //% weight=80
@@ -74,4 +149,4 @@ namespace turtle {
             pins.i2cWriteNumber(89, (4864 + temp_number), NumberFormat.Int16BE)
         }
     }
-}
+} 
