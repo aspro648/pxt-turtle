7ad3d5ba6cb09353eec74ab380e4a450236c4c3d
Maker's Box <648.ken@gmail.com>
2019-09-18 10:48:53 -0700

    Added debugging

--- turtleBot.ts
+++ turtleBot.ts
@@ -16,7 +16,8 @@ namespace turtleBot {
     let steps_rev = 512 //  512 for 64x gearbox, 128 for 16x gearbox
     let x = 0
     let y = 0
-    let heading = 0  // starts to the right (east) (same as Python)
+    let _heading = 0  // starts to the right (east) (same as Python)
+    let debug = true
 
     led.enable(false)
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -41,6 +42,7 @@ namespace turtleBot {
     export function forward(distance: number) {
         // 25 * 512 / (52 * 3.1412)
         steps = Math.round(distance * steps_rev / (wheel_dia * Math.PI))
+        if (debug == true) { serial.writeString("forward(" + distance + ")\n") }
         for (let i = 0; i < steps; i++) {
             pattern = [1, 0, 1, 0]
             pins.digitalWritePin(DigitalPin.P0, pattern[3])
@@ -93,8 +95,8 @@ namespace turtleBot {
         pins.digitalWritePin(DigitalPin.P10, pattern[2])
         pins.digitalWritePin(DigitalPin.P8, pattern[3])
 
-        x = x + distance * Math.cos(heading * Math.PI / 180)  // must convert to radians
-        y = y + distance * Math.sin(heading * Math.PI / 180)
+        x = x + distance * Math.cos(_heading * Math.PI / 180)  // must convert to radians
+        y = y + distance * Math.sin(_heading * Math.PI / 180)
     }
 
 
@@ -104,6 +106,7 @@ namespace turtleBot {
     //% block="backward %distance (mm)" blockId="turtle_backward"
     //% distance.min=0 distance.max=1000 distance.defl=100 group='Control'
     export function backward(distance: number) {
+        if (debug == true) { serial.writeString("backward(" + distance + ")\n") }
         steps = Math.round(distance * steps_rev / (wheel_dia * Math.PI))
         for (let i = 0; i < steps; i++) {
             pattern = [1, 0, 1, 0]
@@ -155,8 +158,8 @@ namespace turtleBot {
         pins.digitalWritePin(DigitalPin.P13, 0)
         pins.digitalWritePin(DigitalPin.P10, 0)
         pins.digitalWritePin(DigitalPin.P8, 0)
-        x = x + distance * Math.cos((heading - 180) * Math.PI / 180)  // must convert to radians
-        y = y + distance * Math.sin((heading - 180) * Math.PI / 180)
+        x = x + distance * Math.cos((_heading - 180) * Math.PI / 180)  // must convert to radians
+        y = y + distance * Math.sin((_heading - 180) * Math.PI / 180)
     }
 
 
@@ -166,6 +169,7 @@ namespace turtleBot {
     //% block="right %distance (degrees)" blockId="turtle_right"
     //% degrees.min=0 degrees.max=360 degrees.defl=90 group='Control'
     export function right(degrees: number) {
+        if (debug == true) { serial.writeString("right(" + degrees + ")\n") }
         rotation = degrees / 360.0
         distance = wheel_base * Math.PI * rotation
         steps = Math.round(distance * steps_rev / (wheel_dia * Math.PI))
@@ -211,11 +215,10 @@ namespace turtleBot {
             pins.digitalWritePin(DigitalPin.P8, pattern[0])
             basic.pause(2)
         }
-        heading = heading - degrees
-        while (heading < 0) {
-            heading = heading + 360
+        _heading = _heading - degrees
+        while (_heading < 0) {
+            _heading = _heading + 360
         }
-        serial.writeValue("heading", heading)
     }
 
 
@@ -225,6 +228,7 @@ namespace turtleBot {
     //% block="left %distance (degrees)" blockId="turtle_left"
     //% degrees.min=0 degrees.max=360 degrees.defl=90 group='Control'
     export function left(degrees: number) {
+        if (debug == true) { serial.writeString("left(" + degrees + ")\n") }
         rotation = degrees / 360.0
         distance = wheel_base * Math.PI * rotation
         steps = Math.round(distance * steps_rev / (wheel_dia * Math.PI))
@@ -270,11 +274,10 @@ namespace turtleBot {
             pins.digitalWritePin(DigitalPin.P8, pattern[0])
             basic.pause(2)
         }
-        heading = heading + degrees
-        while (heading > 360) {
-            heading = heading - 360
+        _heading = _heading + degrees
+        while (_heading > 360) {
+            _heading = _heading - 360
         }
-        serial.writeValue("heading", heading)
     }
 
 
@@ -284,6 +287,7 @@ namespace turtleBot {
     //% block="pen_up()" blockId="pen_up"
     //% group='Control'
     export function pen_up() {
+        if (debug == true) { serial.writeString("penup()\n") }
         servos.P2.setAngle(PEN_UP)
     }
 
@@ -294,21 +298,8 @@ namespace turtleBot {
     //% block="pen_down()" blockId="pen_down"
     //% group='Control'
     export function pen_down() {
+        if (debug == true) { serial.writeString("down()\n") }
         servos.P2.setAngle(PEN_DOWN)
-    }
-
-
-    /**
-     *  Print turtle x,y position to serial console,
-     *  and return array [x, y].
-     */
-    //% block="position()" blockId="position"
-    //% group='Control'
-    export function position(): number[] {
-        serial.writeValue("heading=", heading)
-        serial.writeValue("x=", x)
-        serial.writeValue("y=", y)
-        return [x, y]
     }
 
 
@@ -321,39 +312,55 @@ namespace turtleBot {
     //% block="goto(%gx, %gy)" blockId="goto"
     //% group='Control'
     export function goto(gx: number, gy: number) {
-        serial.writeValue("heading", heading)
         let bearing = getBearing(gx, gy)
-        serial.writeValue("bearning", bearing)
+
         let dist = calc_distance([x, y], [gx, gy])
-        let relBearing = bearing - heading
+        let relBearing = bearing - _heading
         if (relBearing < 0) {
             relBearing = 360 - Math.abs(relBearing)
         }
-        serial.writeValue("relBearing", relBearing)
         if (relBearing <= 90) {
-            serial.writeValue("1 left", relBearing)
-            serial.writeValue("forward", dist)
+            if (debug == true) { serial.writeString("goto(" + gx + ", " + gy + ") Case 1\n") }
             left(relBearing)
             forward(dist)
         }
         else if (relBearing <= 180) {
-            serial.writeValue("2 right", 180 - relBearing)
-            serial.writeValue("backward", dist)
+            if (debug == true) { serial.writeString("goto(" + gx + ", " + gy + ") Case 2\n") }
             right(180 - relBearing)
             backward(dist)
         }
         else if (relBearing <= 270) {
-            serial.writeValue("3 left", relBearing - 180)
-            serial.writeValue("backward", dist)
+            if (debug == true) { serial.writeString("goto(" + gx + ", " + gy + ") Case 3\n") }
             left(relBearing - 180)
             backward(dist)
         }
         else {
-            serial.writeValue("4 right", 360 - relBearing)
-            serial.writeValue("forward", dist)
+            if (debug == true) { serial.writeString("goto(" + gx + ", " + gy + ") Case 4\n") }
             right(360 - relBearing)
             forward(dist)
         }
+    }
+
+
+    /**
+     *  Return turtle position as array [x, y].
+     */
+    //% block="position()" blockId="position"
+    //% group='Control'
+    export function position(): number[] {
+        if (debug == true) { serial.writeString("position = [" + x + ", " + y + "]\n") }
+        return [x, y]
+    }
+
+
+    /**
+     *  Returns turtle heading in degrees.
+     */
+    //% block="heading()" blockId="sheading"
+    //% group='Control'
+    export function heading(): number {
+        if (debug == true) { serial.writeString("heading = " + heading + "\n") }
+        return _heading
     }
 
 
@@ -364,10 +371,11 @@ namespace turtleBot {
 
     function getBearing(gx: number, gy: number) {
         let angle = Math.atan2(gy - y, gx - x) * 180 / Math.PI;  // convert to degrees
-        serial.writeValue("angle", angle)
+        //serial.writeValue("angle", angle)
         let bearing = (angle + 360) % 360
         return bearing
     }
+
 
     /**
      *  Update these value for your turtle's calibration.
@@ -379,5 +387,15 @@ namespace turtleBot {
         wheel_base = wb
         PEN_UP = pu
         PEN_DOWN = pd
+    }
+
+
+    /**
+     *  Turn on logging in console.
+     */
+    //% block="set_debug %Choice"
+    //% blockId="set_debug" group='Parameters'
+    export function set_debug(): void {
+        debug = true
     }
 } 
